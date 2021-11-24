ea08fa39d0deface674435f26a7408cb45db5d62
masalthunlass <masalthunlass@gmail.com>
2021-11-24 13:55:12 +0100

    setup commit

---
+++ src/test/java/kataTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class kataTest {
+
+    @Test
+    void should_work() {
+        assertThat(1).isEqualTo(1);
+    }
+}
