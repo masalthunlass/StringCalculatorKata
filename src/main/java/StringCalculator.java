eb6b6464b498edc790b10a7244cad6f88f136e34
masalthunlass <masalthunlass@gmail.com>
2021-11-27 20:49:39 +0100

    Step 1 - refactoring to be more explicit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,10 +3,27 @@ import java.util.Arrays;
 public class StringCalculator {
 
 
-    int add(String numbers) {
-        if (numbers.isBlank()) return 0;
-        return Arrays.stream(numbers.split(","))
-                .map(Integer::parseInt)
+    public static final String SEPARATOR = ",";
+
+    int add(String commaSeparatedNumbers) {
+        if (commaSeparatedNumbers.isBlank()) return 0;
+        final String[] numbers = commaSeparatedNumbers.split(SEPARATOR);
+        return Arrays.stream(numbers)
+                .map(Operand::new)
+                .map(Operand::getValue)
                 .reduce(0, Integer::sum);
+    }
+
+
+    public static class Operand {
+        private final int value;
+
+        public Operand(String value) {
+            this.value = Integer.parseInt(value);
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 }
